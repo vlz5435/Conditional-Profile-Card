fc9325896e109542697a96113fb8eb96794e7804
vlz5435 <bautistajorge5435@gmail.com>
2020-02-29 16:10:21 +0000

    first commit

--- public/bundle.js
+++ public/bundle.js
@@ -701,7 +701,7 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
@@ -710,21 +710,21 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1> ${variables.name} ${variables.lastname} </h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href=${variables.twitter}><i class="fa fa-twitter"></i></a></li>
+            <li><a href=${variables.github}><i class="fa fa-github"></i></a></li>
+            <li><a href=${variables.linkedin}><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${variables.github}><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
@@ -738,7 +738,7 @@ window.onload = function () {
     socialMediaPosition: "left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
--- src/js/index.js
+++ src/js/index.js
@@ -14,14 +14,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1> ${variables.name} ${variables.lastname} </h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href=${
+              variables.twitter
+            }><i class="fa fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fa fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+            }><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -42,7 +50,7 @@ window.onload = function() {
     socialMediaPosition: "left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
